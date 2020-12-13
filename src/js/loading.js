7e8d3c944a05b83dded8f99856a15ee2c6118328
Jklein64 <jklein.64@verizon.net>
2020-12-13 14:58:27 -0500

    api is bad

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,2 +1,19 @@
 const loader = document.querySelector(".mdc-linear-progress")
 const linearProgress = new window.mdc.linearProgress.MDCLinearProgress(loader)
+
+
+// const data = fetch("https://hacktj2020api.eastbanctech.com/transitiq/Stops?$format=Leg", {
+//     method: "GET",
+//     headers: {
+//         "Ocp-Apim-Subscription-Key": '49310879d5e249dbab5c8db115a51305',
+//     },
+// }).then(raw => {
+//     console.log({
+//         raw
+//     });
+//     return raw.text()
+// }).then(data => {
+//     console.log({
+//         data
+//     })
+// })
