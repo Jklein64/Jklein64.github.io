f514e979fb2d527905674b1a49bc27c6b2d3aa1b
Jklein64 <jklein.64@verizon.net>
2020-12-13 15:29:42 -0500

    try with jquery

--- js/loading.js
+++ js/loading.js
@@ -1,19 +1,47 @@
 const loader = document.querySelector(".mdc-linear-progress")
 const linearProgress = new window.mdc.linearProgress.MDCLinearProgress(loader)
 
+window.$(function () {
+    var params = {
+        // Request parameters
+        "$filter": "{String}",
+        "$top": "{string}",
+        "$skip": "{string}",
+        "$format": "{String}",
+        "$orderby": "{String}",
+    };
 
-const data = fetch("https://hacktj2020api.eastbanctech.com/transitiq/Stops?$format=Leg", {
-    method: "GET",
-    headers: {
-        "Ocp-Apim-Subscription-Key": '49310879d5e249dbab5c8db115a51305',
-    },
-}).then(raw => {
-    console.log({
-        raw
-    });
-    return raw.text()
-}).then(data => {
-    console.log({
-        data
-    })
-})
+    $.ajax({
+            url: "https://hacktj2020api.eastbanctech.com/transitiq/Stops?" + $.param(params),
+            beforeSend: function (xhrObj) {
+                // Request headers
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "49310879d5e249dbab5c8db115a51305");
+            },
+            type: "GET",
+            // Request body
+            data: "{body}",
+        })
+        .done(function (data) {
+            console.log(data)
+            alert("success");
+        })
+        .fail(function () {
+            alert("error");
+        });
+});
+
+// const data = fetch("https://hacktj2020api.eastbanctech.com/transitiq/Stops?$format=Leg", {
+//     method: "GET",
+//     headers: {
+//         "Ocp-Apim-Subscription-Key": '49310879d5e249dbab5c8db115a51305',
+//     },
+// }).then(raw => {
+//     console.log({
+//         raw
+//     });
+//     return raw.text()
+// }).then(data => {
+//     console.log({
+//         data
+//     })
+// })
