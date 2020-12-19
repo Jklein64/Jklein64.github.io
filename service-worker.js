ab606ab6b2a5e64ba8fe52c0c7c852c4ce4b56e0
Jklein64 <jklein.64@verizon.net>
2020-12-19 15:42:42 -0500

    needed to return a promise

--- service-worker.js
+++ service-worker.js
@@ -19,17 +19,34 @@ self.addEventListener("fetch", /** @type {(e: FetchEvent) => void} */ e => {
 
     if (method === "GET" && !url.includes(localOrigin)) {
         // console.log(request.url)
-        // const bad = res => Promise.reject(
-        //     TypeError(`Response is not ok.  Recieved status ${res.status}`))
-
-        // unpkg.com with `?module` can't handle relative `export * from`, and thinks it's absolute.
-        const modified = new Request(request.url.replace("https://unpkg.com/lib", "https://unpkg.com/lit-html@1.3.0/lib"))
-
-        e.respondWith(
-            fetch(modified).then(response => {
-                console.log(response.status, modified, response)
-                return response
-            }))
+        const bad = res => Promise.reject(
+            TypeError(`Response is not ok.  Recieved status ${res.status}`))
+        // console.log("request", request)
+        const modified = new Request(request.url
+            .replace("https://unpkg.com/lib/updating-element.js?module", "https://unpkg.com/lit-element@2.4.0/lib/updating-element.js?module")
+            .replace("https://unpkg.com/lib/css-tag.js?module", "https://unpkg.com/lit-element@2.4.0/lib/css-tag.js?module")
+            .replace("https://unpkg.com/lib/decorators.js?module", "https://unpkg.com/lit-element@2.4.0/lib/decorators.js?module")
+            .replace("https://unpkg.com/blocking-elements@%5E0.1.0?module", "https://unpkg.com/blocking-elements@0.1.1/dist/blocking-elements.js?module")
+            /* .replace(/%5E/g, "") */
+            , {
+                referrer: request.referrer,
+                referrerPolicy: request.referrerPolicy,
+                destination: request.destination,
+                ...request
+            }
+        )
+        console.log(request)
+        // e.respondWith(
+        fetch(request).then(
+            res => res.status === 0 || res.ok ? (() => {
+                console.log(res.status, request, res)
+                return Promise.resolve(res)
+            })() : bad(res),
+            console.error
+        ).catch(reason => {
+            console.error(reason)
+        })
+        // )
     }
 })
 
