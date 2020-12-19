c2d900935a25d2ff2502a7813379f190c9d540a7
Jklein64 <jklein.64@verizon.net>
2020-12-19 17:25:53 -0500

    offline yeet

--- service-worker.js
+++ service-worker.js
@@ -3,51 +3,47 @@ self.addEventListener("install", e => {
     console.log("WORKER: install event in progress.")
     e.waitUntil(
         caches
-        .open(`${version}::fundamentals`)
+        .open(`${version}::internal`)
         .then(cache => cache.addAll([
+            "",
             "/",
-            "/js/common/deps.js"
+            "index.html",
+            "js/common/deps.js",
+            "js/common/barba.js",
+            "js/common/state.js",
+            "js/index/destinations.js",
+            "js/index/drawer.js",
+            "js/index/fab.js",
+            "js/index/range.js",
         ]))
         .then(() => console.log("WORKER: install completed"))
     )
 })
 
-self.addEventListener("fetch", /** @type {(e: FetchEvent) => void} */ e => {
+self.addEventListener("fetch", /** @type {(e: FetchEvent) => void} */ async e => {
     // console.log("WORKER: fetch event in progress.")
     const { request } = e, { method, url } = request
     const localOrigin = globalThis.location.origin
 
-    if (method === "GET" && !url.includes(localOrigin)) {
-        // console.log(request.url)
-        const bad = res => Promise.reject(
-            TypeError(`Response is not ok.  Recieved status ${res.status}`))
-        // console.log("request", request)
-        const modified = new Request(request.url
-            .replace("https://unpkg.com/lib/updating-element.js?module", "https://unpkg.com/lit-element@2.4.0/lib/updating-element.js?module")
-            .replace("https://unpkg.com/lib/css-tag.js?module", "https://unpkg.com/lit-element@2.4.0/lib/css-tag.js?module")
-            .replace("https://unpkg.com/lib/decorators.js?module", "https://unpkg.com/lit-element@2.4.0/lib/decorators.js?module")
-            .replace("https://unpkg.com/blocking-elements@%5E0.1.0?module", "https://unpkg.com/blocking-elements@0.1.1/dist/blocking-elements.js?module")
-            /* .replace(/%5E/g, "") */
-            , {
-                referrer: request.referrer,
-                referrerPolicy: request.referrerPolicy,
-                destination: request.destination,
-                ...request
-            }
-        )
-        console.log(request)
-        // e.respondWith(
-        fetch(request).then(
-            res => res.status === 0 || res.ok ? (() => {
-                console.log(res.status, request, res)
-                return Promise.resolve(res)
-            })() : bad(res),
-            console.error
-        ).catch(reason => {
-            console.error(reason)
-        })
-        // )
-    }
+    console.log(`asking for ${url}`)
+
+    // if (method === "GET" && !url.includes(localOrigin)) {
+    e.respondWith(async function() {
+        const cache = await caches.open(`${version}::external`)
+        const cached = await cache.match(request)
+
+        if (cached) {
+            console.log(`WORKER: Serving ${request.url} from cache.`)
+            return cached
+        } else {
+            console.log(`WORKER: Cache miss on ${request.url}.`)
+            const response = await fetch(request)
+            cache.put(request, response.clone())
+            console.log(response)
+            return response
+        }
+    }())
+    // }
 })
 
 self.addEventListener("activate", e => {
