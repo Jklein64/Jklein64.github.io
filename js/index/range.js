8587465d36fa7f140d88fe6374bab732f2e798f9
Jklein64 <jklein.64@verizon.net>
2020-12-19 08:56:56 -0500

    allow for different miles text

--- js/index/range.js
+++ js/index/range.js
@@ -1,25 +1,32 @@
 import state from "../common/state.js"
 
-const dialog = document.getElementById("range-dialog").firstElementChild
-const distance = dialog.querySelector(".distance")
+// Range slider
 const slider = document.getElementById("range-slider")
+const distance = dialog.querySelector(".distance")
+
+slider.addEventListener("input", e => setRangeContent(e.target.value))
+state.subscribe("range", setRangeContent)
+
+function setRangeContent(range) {
+    distance.textContent = range
+    const miles = distance.nextElementSibling
+    miles.textContent = range > 1 ? "miles" : "mile"
+}
+
+
+// Range dialog
+const dialog = document.getElementById("range-dialog").firstElementChild
 const confirm = dialog.querySelector("mwc-button[slot*='primary']")
 const cancel = dialog.querySelector("mwc-button[slot*='secondary']")
 
-slider.addEventListener("input", e => {
-    distance.textContent = e.target.value
-})
-
 confirm.addEventListener("click", () => state.range = parseInt(distance.textContent))
-cancel.addEventListener("click", () => {
-    distance.textContent = state.range
-    slider.value = state.range
-})
+cancel.addEventListener("click", () => (distance.textContent = state.range, slider.value = state.range))
 
+dialog.addEventListener("closed", () => state.rangeDialogOpen = false)
 dialog.addEventListener("opened", function oneShot() {
     dialog.removeEventListener("opened", oneShot)
+    state.range = slider.getAttribute("value")
     slider.layout()
 })
 
 state.subscribe("rangeDialogOpen", openState => dialog.open = openState)
-dialog.addEventListener("closed", () => state.rangeDialogOpen = false)
